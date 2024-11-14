3228cc09bd58df63d00da3cc43749b4066cfd317
Soumik205 <soumikghosh205@gmail.com>
2024-11-15 02:34:23 +0600

    feat: user authentication schemas

---
+++ libs/schemas.js
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+export const signUpSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address"),
+  phone: z
+    .string()
+    .min(10, "Phone number must be at least 10 digits")
+    .regex(/^\d+$/, "Phone number must contain only numbers"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
