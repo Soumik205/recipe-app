a8f9b28f2664e6218678eac2ec013e5ed48cbc19
Soumik205 <soumikghosh205@gmail.com>
2024-11-14 22:02:00 +0600

    fix: single recipe modal

--- common/helpers/HttpKit.js
+++ common/helpers/HttpKit.js
@@ -39,11 +39,10 @@ const HttpKit = {
 
   getRecipeDetails: async (id) => {
     try {
-      const response = axios
+      const response = await axios
         .get(`${BASE_URL}/lookup.php`, {
           params: { i: id },
         })
-        .then((res) => res);
       return response.data.meals ? response.data.meals[0] : null;
     } catch (error) {
       console.error("Error fetching recipe details:", error);
